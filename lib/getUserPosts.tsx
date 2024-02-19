14bebf63de887db408e6126f8930ddad9b56d0b0
Vishal-Chandrasekaran <vishalchandrasekar@bitcot.com>
2024-02-19 14:45:15 +0530

    third video about rendering and notFound

--- lib/getUserPosts.tsx
+++ lib/getUserPosts.tsx
@@ -1,5 +1,10 @@
 export default async function getUserPosts( userId: string ) {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`);
-    if(!res.ok) throw new Error ('failed to fetch user post')
+    //using cache as 'force-cache' which is actually default 
+    //in order to avoid stale data 'no-store'
+    //Now for ISR,
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`,{next: {revalidate:60}});
+    // We are not throwing error as we are about to create a 404 page
+    // if(!res.ok) throw new Error ('failed to fetch user post')
+    if(!res.ok) return undefined
     return res.json();
 };
